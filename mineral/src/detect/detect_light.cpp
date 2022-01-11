f8cb782364992c21575fd5d5d2e761a603d1138a
lykycy123 <1336857701@qq.com>
2022-01-11 19:42:07 +0800

    get light

--- mineral/src/detect/detect_light.cpp
+++ mineral/src/detect/detect_light.cpp
@@ -4,17 +4,44 @@ void Mineral::detectlight(const Mat &operand,const Mat &preimage)
 {
     if (initMineral())
     {
-        //形态学操作+HSV颜色分离
-        Mat dst(operand.size(), CV_8UC3);
-        Morlight(operand, dst);
 
-        Mat predst(preimage.size(), CV_8UC3);
-        Morlight(preimage, predst);
+        //找到闪烁灯光
+        Mat dst;
+        getRightLight(operand,preimage,dst);
+
+
+#ifdef DEBUG
+        Mat BackGround = Mat::zeros(operand.size(), CV_8UC3);
+        imshow("after Mor", dst);
+        waitKey(1);
+#endif
+
+        //找轮廓
+        getlightcontours(dst,BackGround);
+
+        
+        //对各矩形取中心点
+        getFitPoints(all_fit_rects, all_fit_points);
+
+
+        //对点集进行排序
+        sortPointsVector(all_fit_points, X_INCREASE);
+
+        Number_yellow=all_fit_points.size();
+
+
+        //发送数据
+        sendTarget();
 
 
         
+        //调试
+        for (auto item : all_fit_points)
+        circle(BackGround, item, 2, Scalar(0, 255, 0), 2);
 
-    
+        cout << "Number light:" << Number_yellow << endl;
+        imshow("result_light", BackGround);
+        waitKey(1);
 
 
 
